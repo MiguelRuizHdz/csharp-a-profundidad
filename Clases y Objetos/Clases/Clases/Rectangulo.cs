39636d68930a50bab2dad0acb4af678b16b002a5
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-05-13 17:40:55 -0500

    Clases y Objetos

---
+++ Clases y Objetos/Clases/Clases/Rectangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class Rectangulo
+    {
+        // propiedades o atributos
+        public int Base { get; set; }
+        public int Altura { get; set; }
+
+        // métodos
+        public int CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+}
---
+++ Clases y Objetos/ClasesObjetos/ClasesObjetos/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClasesObjetos
+{
+    public partial class Form1 : Form
+    {
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Triangulo
+    {
+        // los atributos son las variables que se declaran dentro de la clase
+    }
+}
