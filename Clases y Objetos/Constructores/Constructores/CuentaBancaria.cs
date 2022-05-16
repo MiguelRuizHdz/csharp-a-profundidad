ed04b541b91fdc4694a0551bfb4b1b0396347fcc
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-05-16 15:07:14 -0500

    Constructores

---
+++ Clases y Objetos/Constructores/Constructores/CuentaBancaria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    internal class CuentaBancaria
+    {
+        //propiedades
+        public string NoCuenta {  get; set; }
+        public string Usuario { get; set; }
+        public decimal Saldo { get; set; }
+
+        //Constructor vacío o por default
+        public CuentaBancaria() { }
+        public CuentaBancaria(string noCuenta)
+        {
+            NoCuenta = noCuenta;
+        }
+        // se le piden parámetros al momento de instanciar una clase para asignarlos al atributo correspondiente.
+        public CuentaBancaria(string noCuenta, string usuario) : this(noCuenta)
+        {
+            Usuario = usuario;
+        }
+        // constructor que inicializa todas las variables
+        public CuentaBancaria(string noCuenta, string usuario, decimal saldo) : this(noCuenta, usuario)
+        {
+            Saldo = saldo;
+        }
+    }
+}
--- Clases y Objetos/Constructores/Constructores/Program.cs
+++ Clases y Objetos/Constructores/Constructores/Program.cs
@@ -10,6 +10,22 @@ namespace Constructores
     {
         static void Main(string[] args)
         {
+            // Usando el constructor con un argumento
+            CuentaBancaria cuenta1 = new CuentaBancaria("000001");
+            Console.WriteLine("NoCuenta: {0}, Usuario: {1}, Saldo: {2}", cuenta1.NoCuenta,
+                cuenta1.Usuario, cuenta1.Saldo);
+
+            // Usando el constructor con dos argumentos
+            CuentaBancaria cuenta2 = new CuentaBancaria("000002", "John Doe");
+            Console.WriteLine("NoCuenta: {0}, Usuario: {1}, Saldo: {2}", cuenta2.NoCuenta,
+                cuenta2.Usuario, cuenta2.Saldo);
+
+            // Usando el constructor con tres argumentos
+            CuentaBancaria cuenta3 = new CuentaBancaria("000002", "Jane Doe", 10000);
+            Console.WriteLine("NoCuenta: {0}, Usuario: {1}, Saldo: {2}", cuenta3.NoCuenta,
+                cuenta3.Usuario, cuenta3.Saldo);
+
+            Console.ReadKey();
         }
     }
 }
