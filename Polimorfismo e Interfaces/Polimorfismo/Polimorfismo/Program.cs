bf1515f61ea53b5bdd5eaaa977ba75e6a1524d28
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-05-18 13:28:23 -0500

    Polimorfismo -Virtual y Override

--- Polimorfismo e Interfaces/Polimorfismo/Polimorfismo/Program.cs
+++ Polimorfismo e Interfaces/Polimorfismo/Polimorfismo/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Polimorfismo
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Animal miAnimal = new Animal();  // Crea un objeto Animal
+            Animal miGato = new Gato();  // Crea un objeto Gato
+            Animal miPerro = new Perro();  // Crea un objeto Perro
+
+            miAnimal.sonidoAnimal();
+            miGato.sonidoAnimal();
+            miPerro.sonidoAnimal();
+
+            Console.ReadKey();
         }
     }
+
+    class Animal // Clase base (padre)
+    {
+        public virtual void sonidoAnimal()
+        {
+            Console.WriteLine("El animal hace un sonido");
+        }
+    }
+
+    class Gato : Animal // Clase derivada (hijo)
+    {
+        public override void sonidoAnimal()
+        {
+            Console.WriteLine("El gato dice: miau miau");
+        }
+    }
+
+    class Perro : Animal // Clase derivada (hijo)
+    {
+        public override void sonidoAnimal()
+        {
+            Console.WriteLine("El perro dice: guau guau");
+        }
+    }
+
 }
