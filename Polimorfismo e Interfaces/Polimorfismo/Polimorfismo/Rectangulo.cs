a95d521a430af928f26737716f583a24885b79be
Miguel Angel Ruiz Hernández <miguel.ruizhz@uanl.edu.mx>
2022-05-17 19:38:51 -0500

    Polimorfismo e Interfaces

---
+++ Polimorfismo e Interfaces/Polimorfismo/Polimorfismo/Rectangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Rectangulo : IFigura
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea()
+        {
+            return Base * Altura;
+        }
+
+        public void DatosRectangulo()
+        {
+            Console.WriteLine("Base: {0}, Altura: {1}", Base, Altura);
+        }
+    }
+}
---
+++ Polimorfismo e Interfaces/Polimorfismo/Polimorfismo/Triangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Triangulo:IFigura 
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea()
+        {
+            return (Base * Altura)/2;
+        }
+
+        public void DatosTriangulo()
+        {
+            Console.WriteLine("Base: {0}, Altura: {1}", Base, Altura);
+        }
+    }
+}
