810d5c4dc43a9cf88a21e3f4857078ba15b5dbcb
Miguel Angel Ruiz Hernández <miguel.ruizhz@uanl.edu.mx>
2022-05-18 00:44:00 -0500

    Clases Abstractas

---
+++ Polimorfismo e Interfaces/ClasesAbstractas/ClasesAbstractas/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+
+        public Persona() { }
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ( obj == null)
+            {
+                return false;
+            }
+
+            // intenta castear
+            Persona p = obj as Persona;
+            if(p == null)
+            {
+                return false;
+            }
+
+            return (Nombre == p.Nombre) && (Edad == p.Edad);
+        }
+    }
+}
---
+++ Polimorfismo e Interfaces/ClasesAbstractas/ClasesAbstractas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract void sonidoAnimal();
+        public void dormir()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+}
