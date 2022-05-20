9db5f6525fb0f55633ec0da00b002228da8c0567
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-05-20 15:42:49 -0500

    operadores

---
+++ Operadores/Operadores/Operadores/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operador de asignación
+            int x = 5;
+            x += 10;
+            // Operador +
+            int suma1 = 20 + 50;        // 70 (20 + 50)
+            int suma2 = suma1 + 100;      // 170 (70 + 100)
+            int suma3 = suma2 + suma2;     // 340 (170 + 170)
+        }
+    }
+}
