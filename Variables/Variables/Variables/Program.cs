a52096246776dff805a93cb41194029fb063ad71
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2022-05-20 14:20:12 -0500

    Variables

--- Variables/Variables/Variables/Program.cs
+++ Variables/Variables/Variables/Program.cs
@@ -10,5 +10,24 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            int numero = 10;
+            // Operación multiplicación
+            int resultado = numero * 5;
+            // Imprimimos el resultado en consola
+            Console.WriteLine(resultado);
+            bool vivo = true;
+            byte miByte = 255;
+            sbyte miSByte = 127;
+            char caracter = 'C';
+            long numeroLargo = 90192910290219;
+            float precio = 9.99F;
+            double total = 5.99;
+            decimal total2 = 9.99M;
+            int suma = 8 + 20;
+            string cadena = "Este es un texto";
+
+            // Espera a que presionemos una tecla. Se cerrará automaticamente sino agregamos la siguiente línea
+            Console.ReadKey();
         }
- 
+    }
+}
